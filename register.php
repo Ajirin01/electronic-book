d7787236427610dd2bce0558b6c1adf552c31259
Ajirin01 <mubarakolagoke@gmail.com>
2023-11-10 01:48:39 +0100

    nav bar unified for add pages

--- register.php
+++ register.php
@@ -68,72 +68,7 @@ $contents = json_decode(file_get_contents("contents.json"));
     <!-- header section strats -->
     <header class="header_section">
       <div class="container">
-        <nav class="navbar navbar-expand-lg custom_nav-container pt-3">
-          <a class="navbar-brand" href="index.html">
-            <img src="images/Electronic_logo.png"  style="width: 45px; height: 44.5px;" /><span>
-              <!-- Tropiko -->
-              Electronic
-            </span>
-          </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
-              <ul class="navbar-nav  ">
-                <li class="nav-item active">
-                  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                </li>
-
-                <li class="nav-item active">
-                  <div class="btn-group" style="margin-top: 8px">
-                      <a href="#" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">Resources</a>
-                      <div class="dropdown-menu dropdown-menu-right">
-                          <button class="dropdown-item" type="button" data-toggle="modal" data-target="#constantsModal">Physical Constants</button>
-                          <button class="dropdown-item" type="button" data-toggle="modal" data-target="#calculatorModal">Scientific Calculator</button>
-                      </div>
-                  </div>
-                </li>
-                
-                <li class="nav-item active">
-                  <a class="nav-link" data-toggle="modal" data-target="#contentMenuModal" href="#Contents">Contents <span class="sr-only">(current)</span></a>
-                </li>
-
-                <!-- <li class="nav-item active" style="margin-left: 5px">
-                  <div class="btn-group" style="margin-top: 8px">
-                      <a href="#" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">Contents</a>
-                      <div class="dropdown-menu dropdown-menu-right">
-                          <button class="dropdown-item" type="button" data-toggle="modal" data-target="#constantsModal">Physical Constants</button>
-                          <button class="dropdown-item" type="button" data-toggle="modal" data-target="#calculatorModal">Scientific Calculator</button>
-                      </div>
-                  </div>
-                </li> -->
-                
-                
-                <!-- <li class="nav-item">
-                  <a class="nav-link" href="fruit.html"> Books</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="service.html"> Services </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="contact.html">Contact us</a>
-                </li> -->
-              </ul>
-              <!-- <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
-                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
-              </form> -->
-            </div>
-            <div class="ml-0 ml-lg-4 d-flex justify-content-center">
-              <a href="" class="btn px-0 ml-3">
-                  <i class="fas fa-shopping-cart text-primary"  style="color: black !important"></i>
-                  <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px; color: black !important; border-color: black !important;">0</span>
-              </a>
-            </div>
-          </div>
-        </nav>
+        <?php include_once("nav-bar-component.php") ?>
       </div>
     </header>
     <!-- end header section -->
